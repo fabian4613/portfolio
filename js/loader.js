affa6cc2e07d53f45d5d41ef1b59dec789c9559c
Fabián Pavia <68711515+fabian4613@users.noreply.github.com>
2021-12-15 01:25:35 -0300

    Create loader.js

---
+++ js/loader.js
@@ -0,0 +1,16 @@
+const loader = document.querySelector('.lds-dual-ring');
+const main = document.querySelector('.main');
+const header = document.querySelector('.header');
+const body = document.querySelector('.body');
+
+function init() {
+    /*Carga de loader de inicio y transicion-delay a la web*/
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+        main.style.display = 'block';
+        header.style.display = 'block';
+        body.style.background = "var(--body-color)";
+    }, 3000);
+}
+init();
